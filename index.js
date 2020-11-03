4964e8e5c9bb1b32d3d1881f494b96ddf7967e70
hojin Mun <jin1281@gmail.com>
2020-11-03 10:03:04 -0500

    functions

--- index.js
+++ index.js
@@ -10,34 +10,66 @@
 //   }
 // }
 
-const str = " hello";
-let newStr = "";
-for (let i = str.length - 1; i >= 0; i--) {
-  newStr += str[i];
-}
-console.log(newStr);
+// const str = " hello";
+// let newStr = "";
+// for (let i = str.length - 1; i >= 0; i--) {
+//   newStr += str[i];
+// }
+// console.log(newStr);
 
-const msg = "Hello im learning to be a web developer";
-const newstr = msg.split(" ").reverse().join().replace(/,/g, " ");
+// const msg = "Hello im learning to be a web developer";
+// const newstr = msg.split(" ").reverse().join().replace(/,/g, " ");
 
-console.log(newstr);
+// console.log(newstr);
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let max = arr[0];
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let max = arr[0];
 
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] >= max) {
-    max = arr[i];
-   console.log('setting max to ' + max);
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] >= max) {
+//     max = arr[i];
+//    console.log('setting max to ' + max);
+//   }
+// }
+// console.log(max);
+// let min = arr[0];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] <= min) {
+//     min = arr[i];
+//    console.log('setting min to ' + min);
+//   }
+// }
+// console.log(min);
+
+
+function reverseString(str) {
+  var newString = "";
+  for (var i = str.length - 1; i >= 0; i--) {
+      newString += str[i];
   }
+  return newString;
 }
-console.log(max);
-let min = arr[0];
+reverseString('hello');
 
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] <= min) {
-    min = arr[i];
-   console.log('setting min to ' + min);
-  }
+
+// function reverseSentence(sentence) {
+//   const words = sentence.split(" ");
+//   const reversed = words.reverse();
+//   const result = reversed.join(" ");
+//   return result;
+// }
+
+
+
+var reverseWords = function(sentence){
+
+	var words = sentence.split(" ").reverse(); 
+	var string = "";
+	for(word in words)
+		string += (word > 0 ? " " : "") + words[word]; 
+	return string;
+	
 }
-console.log(min);
+reverseWords('I am learning to be a web developer')
+
